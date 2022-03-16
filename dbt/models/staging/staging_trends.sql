d1510d041fa4d8af3cb2dc1aabaed0081506004d
Carlos A. Crespo <lvccrespo@gmail.com>
2022-03-15 22:14:49 -0300

    add trends model

---
+++ dbt/models/staging/staging_trends.sql
@@ -0,0 +1,11 @@
+{{ config(materialized='table') }}
+
+SELECT
+geoCode,
+value,
+CAST(SUBSTR(cast(date as string), 0, 4) AS INT) as year,
+CAST(SUBSTR(cast(date as string), 5, 2) AS INT) as month, 
+CAST(SUBSTR(cast(date as string), 7, 2) AS INT) as day,
+hl,
+row_number()  over (partition by hl, date order by value desc) as rank
+FROM {{source('staging'.'trends_external_table')}}
