035672caef20ef6a3ab9eb166a7066e805887b88
Carlos A. Crespo <lvccrespo@gmail.com>
2022-03-15 23:44:49 -0300

    cast date in staging

--- dbt/models/staging/staging_trends.sql
+++ dbt/models/staging/staging_trends.sql
@@ -1,12 +1,12 @@
 {{ config(materialized='view') }}
 
 SELECT
-geoCode,
-value, 
 DATE(
 CAST(SUBSTR(cast(date as string), 0, 4) AS INT),
 CAST(SUBSTR(cast(date as string), 5, 2) AS INT), 
 CAST(SUBSTR(cast(date as string), 7, 2) AS INT)) as date,
+geoCode,
+value, 
 hl,
 row_number()  over (partition by hl, date order by value desc) as rank
 FROM {{source('staging','trends_external_table')}}
