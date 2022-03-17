aec3dc60c31d0df8475915e773a2c66a8b3053da
Carlos A. Crespo <lvccrespo@gmail.com>
2022-03-17 21:50:32 +0000

    fix model top_hl (only select)

--- dbt/models/core/top_hl_trends.sql
+++ dbt/models/core/top_hl_trends.sql
@@ -1,17 +1,6 @@
+-- TODO: Refactor for loop
 {{ config(materialized='table') }}
 
-DECLARE codes ARRAY<STRUCT<country STRING, code_hl STRING>>;
-DECLARE dsql string;
-
-
-set codes = (
-[('AR','es-AR'),
-('MX','es-MX'),
-('ES','es-ES'),
-('US','en-US'),
-('DE','de-DE')]);
-
-SET dsql = """
 (SELECT 
 date,
 geoCode,
@@ -20,21 +9,59 @@ rank
   FROM 
     {{ref('stg_trends')}} 
   WHERE 
-    geoCode = '%s'
-    AND hl = '%s'
+    geoCode = 'AR'
+    AND hl = 'es-AR'
   ORDER by 
-    date DESC, hl ASC)
-""";
-
-EXECUTE IMMEDIATE(
-select 
-format(dsql, codes[OFFSET(0)].country, codes[OFFSET(0)].code_hl)
-|| 'UNION ALL' ||
-format(dsql, codes[OFFSET(1)].country, codes[OFFSET(1)].code_hl)
-|| 'UNION ALL' ||
-format(dsql, codes[OFFSET(2)].country, codes[OFFSET(2)].code_hl)
-|| 'UNION ALL' ||
-format(dsql, codes[OFFSET(3)].country, codes[OFFSET(3)].code_hl)
-|| 'UNION ALL' ||
-format(dsql, codes[OFFSET(4)].country, codes[OFFSET(4)].code_hl)
-)
+    date DESC, geoCode ASC)
+UNION ALL
+(SELECT 
+date,
+geoCode,
+value,
+rank
+  FROM 
+    {{ref('stg_trends')}} 
+  WHERE 
+    geoCode = 'ES'
+    AND hl = 'es-ES'
+  ORDER by 
+    date DESC, geoCode ASC)
+UNION ALL
+(SELECT 
+date,
+geoCode,
+value,
+rank
+  FROM 
+    {{ref('stg_trends')}} 
+  WHERE 
+    geoCode = 'MX'
+    AND hl = 'es-MX'
+  ORDER by 
+    date DESC, geoCode ASC)
+UNION ALL
+(SELECT 
+date,
+geoCode,
+value,
+rank
+  FROM 
+    {{ref('stg_trends')}} 
+  WHERE 
+    geoCode = 'US'
+    AND hl = 'en-US'
+  ORDER by 
+    date DESC, geoCode ASC)
+UNION ALL
+(SELECT 
+date,
+geoCode,
+value,
+rank
+  FROM 
+    {{ref('stg_trends')}} 
+  WHERE 
+    geoCode = 'DE'
+    AND hl = 'de-DE'
+  ORDER by 
+    date DESC, geoCode ASC)
