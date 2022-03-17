88f10e4b9452af5c58e3c8f921f32ef9c55b4f46
Carlos A. Crespo <lvccrespo@gmail.com>
2022-03-17 18:53:45 -0300

    add model top_hl_trends

---
+++ dbt/models/core/top_hl_trends.sql
@@ -0,0 +1,81 @@
+-- TODO: Refactor for loop
+{{ config(materialized = 'table') }} (
+  SELECT 
+    date, 
+    geoCode, 
+    value, 
+    rank 
+  FROM 
+    {{ref('stg_trends') }} 
+  WHERE 
+    geoCode = 'AR' 
+    AND hl = 'es-AR' 
+  ORDER by 
+    date DESC, 
+    geoCode ASC
+) 
+UNION ALL 
+  (
+    SELECT 
+      date, 
+      geoCode, 
+      value, 
+      rank 
+    FROM 
+      {{ref('stg_trends') }} 
+    WHERE 
+      geoCode = 'ES' 
+      AND hl = 'es-ES' 
+    ORDER by 
+      date DESC, 
+      geoCode ASC
+  ) 
+UNION ALL 
+  (
+    SELECT 
+      date, 
+      geoCode, 
+      value, 
+      rank 
+    FROM 
+      {{ref('stg_trends') }} 
+    WHERE 
+      geoCode = 'MX' 
+      AND hl = 'es-MX' 
+    ORDER by 
+      date DESC, 
+      geoCode ASC
+  ) 
+UNION ALL 
+  (
+    SELECT 
+      date, 
+      geoCode, 
+      value, 
+      rank 
+    FROM 
+      {{ref('stg_trends') }} 
+    WHERE 
+      geoCode = 'US' 
+      AND hl = 'en-US' 
+    ORDER by 
+      date DESC, 
+      geoCode ASC
+  ) 
+UNION ALL 
+  (
+    SELECT 
+      date, 
+      geoCode, 
+      value, 
+      rank 
+    FROM 
+      {{ref('stg_trends') }} 
+    WHERE 
+      geoCode = 'DE' 
+      AND hl = 'de-DE' 
+    ORDER by 
+      date DESC, 
+      geoCode ASC
+  )
+
