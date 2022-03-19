76dd0bd9c163771637af8aea763257c7d95c3f87
Carlos A. Crespo <lvccrespo@gmail.com>
2022-03-19 08:33:35 -0300

    staging topics, queries. Minus fix top_trends

--- dbt/models/core/top_trends.sql
+++ dbt/models/core/top_trends.sql
@@ -3,10 +3,11 @@
   schema='core'
 ) }}
 
-WITH ranking as (
-  SELECT 
-    distinct date, 
-    rank 
+SELECT 
+    date,
+    rank,
+    value,
+    hl
   FROM 
     {{ref('stg_trends')}} 
   WHERE 
@@ -14,38 +15,3 @@ WITH ranking as (
   ORDER by 
     date DESC, 
     rank ASC
-) 
-SELECT 
-  a.*, 
-  b.geoCode AS geo_MX, 
-  b.value AS value_MX, 
-  c.geoCode AS geo_US, 
-  c.value AS value_US, 
-  d.geoCode AS geo_ES, 
-  d.value AS value_ES, 
-  e.geoCode AS geo_AR, 
-  e.value AS value_AR, 
-  f.geoCode AS geo_DE, 
-  f.value AS value_DE 
-from 
-  ranking a 
-  LEFT JOIN {{ref('stg_trends')}} b on a.date = b.date 
-  AND a.rank = b.rank 
-  LEFT JOIN {{ref('stg_trends')}} c on a.date = c.date 
-  AND a.rank = c.rank 
-  LEFT JOIN {{ref('stg_trends')}} d on a.date = d.date 
-  AND a.rank = d.rank 
-  LEFT JOIN {{ref('stg_trends')}} e on a.date = e.date 
-  AND a.rank = e.rank 
-  LEFT JOIN {{ref('stg_trends')}} f on a.date = f.date 
-  AND a.rank = f.rank 
-WHERE 
-  b.hl = 'es-MX' 
-  AND c.hl = 'en-US' 
-  AND d.hl = 'es-ES' 
-  AND e.hl = 'es-AR' 
-  AND f.hl = 'de-DE' 
-ORDER BY 
-  date DESC, 
-  rank ASC
-
