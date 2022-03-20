fad270640fa787f7f0b5efda70162f21a95047bb
Carlos A. Crespo <lvccrespo@gmail.com>
2022-03-20 12:14:59 -0300

    hl top queries

--- dbt/models/core/top_queries_rising.sql
+++ dbt/models/core/top_queries_rising.sql
@@ -4,7 +4,13 @@
 
 SELECT 
     date,
-    geo,
+    CASE geo -- with this fix key value to join tables
+        WHEN 'AR' THEN 'es-AR'
+        WHEN 'ES' THEN 'es-ES'
+        WHEN 'MX' THEN 'es-MX'
+        WHEN 'US' THEN 'en-US'
+        WHEN 'DE' THEN 'de-DE'                            
+        END as hl,
     query,
     value,
     rank
